3556a5b4cb8596c356e5fbdbb759e8370dd6d554
Z User <z@container>
2026-07-13 07:59:38 +0000

    Fix build: settings.gradle, compose plugin, wrapper jar
    - settings.gradle.kts: dependencyResolution -> dependencyResolutionManagement
    - Remove org.jetbrains.kotlin.plugin.compose (not available in Kotlin 1.9.22)
    - Add composeOptions { kotlinCompilerExtensionVersion = "1.5.8" } instead
    - Replace gradle-wrapper.jar with correct bootstrap jar (extracted inner jar)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
 
@@ -58,6 +57,10 @@ android {
 
     buildFeatures {
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
 }
 
